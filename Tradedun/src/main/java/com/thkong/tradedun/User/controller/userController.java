765d1de8b62e56d9d89922150370517a6472afae
tony950620 <tony950620@gmail.com>
2019-10-21 02:01:54 +0900

    20191021 - 로그아웃 구현, header.jsp에 로그인 버튼 추가

--- Tradedun/src/main/java/com/thkong/tradedun/User/controller/userController.java
+++ Tradedun/src/main/java/com/thkong/tradedun/User/controller/userController.java
@@ -28,8 +28,18 @@ public class userController {
 	public String kakaoLogin(@RequestParam(name = "code", required = true) String code, HttpServletRequest req) throws IOException {
 		
 		String redirectUrl = req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort();
+		
+		//로그인 및 회원가입한 사용자의 id를 세션에 담아 home으로 리다렉트 해준다.
 		req.getSession().setAttribute("user", service.kakaoLogin(code, redirectUrl));
 		
-		return null;
+		return "redirect:/";
 	}
+
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String login(HttpServletRequest req) {
+		//로그아웃시 모든 세션을 제거해준다.
+		req.getSession().invalidate();
+		return "redirect:/";
+	}
+	
 }
