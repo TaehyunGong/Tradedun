493bf983847d04c4bc271c4ea1e9f44ac41517d8
tony950620 <tony950620@gmail.com>
2019-11-01 17:21:49 +0900

    20191101 - 이미지 저장 test코드

--- Tradedun/src/test/java/com/thkong/tradedun/main.java
+++ Tradedun/src/test/java/com/thkong/tradedun/main.java
@@ -1,25 +1,40 @@
 package com.thkong.tradedun;
 
+
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 
-import org.codehaus.jackson.map.ObjectMapper;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
-
-import com.thkong.tradedun.Auction.vo.AuctionCharacterDetail;
-import com.thkong.tradedun.Common.DnfApiLib;
+import javax.imageio.ImageIO;
 
 public class main {
 
+	public void saveImage() {
+		File outputFile = new File("C:\\Users\\John\\Desktop\\테스트\\images");
+		 
+		URL url = null;
+		BufferedImage bi = null;
+		        
+		try {
+		    url = new URL("https://img-api.neople.co.kr/df/servers/bakal/characters/669544118444dcfa72786e392e09304e");
+		    bi = ImageIO.read(url);
+		    ImageIO.write(bi, "png", outputFile);
+		}
+		catch(Exception ex) {
+			
+		}
+		System.out.println("완료");
+	}
+	
 	public static void main(String[] args) throws IOException {
 		
-		ApplicationContext context = new GenericXmlApplicationContext("/SpringConfig/config/root-context.xml");
+//		ApplicationContext context = new GenericXmlApplicationContext("/SpringConfig/config/root-context.xml");
 //		ApplicationContext sessionContext = new GenericXmlApplicationContext("/SpringConfig/config/database-context.xml");
-		DnfApiLib dnfapi = context.getBean("dnfapi", DnfApiLib.class);
+//		DnfApiLib dnfapi = context.getBean("dnfapi", DnfApiLib.class);
 //		httpConnection conn = context.getBean("conn", httpConnection.class);
 //		SqlSessionTemplate session = sessionContext.getBean("sqlSession", SqlSessionTemplate.class);
-		ObjectMapper mapper = context.getBean("mapper", ObjectMapper.class);
-		AuctionCharacterDetail detail = dnfapi.charactersAvatar("bakal", "43555446f17e37fcf3eb546ee312d0f8");
-		System.out.println(detail);
+		
+		new main().saveImage();
 	}
 }
