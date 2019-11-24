46379fb7ce64d664b8f3ebb0a8a0944b76a0e128
tony9 <tony9@DESKTOP-TOT5IC4>
2019-11-24 23:17:51 +0900

    20191124 - 아바타 리스트에서 직군과 아바타명 보이도록 수정

--- Tradedun/src/main/java/com/thkong/tradedun/Auction/vo/AuctionSalesBoard.java
+++ Tradedun/src/main/java/com/thkong/tradedun/Auction/vo/AuctionSalesBoard.java
@@ -9,11 +9,14 @@ public class AuctionSalesBoard {
 	
 	private String boardNo;
 	private String subject;
+	private String comment;
 	private String jobName;
+	private String jobGrowId;
 	private String jobGrowName;
-	private String imageName;
+	private String imageName; 
 	private String totalPrice;
 	private String category;
+	private String categoryName;
 	private String charBox;
 	private Date createDT;
 	
