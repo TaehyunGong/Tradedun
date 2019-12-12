f8de0c6fb3bb409cd170d57e4cf3c8dae0c5cbaf
tony <tony950620@naver.com>
2019-12-12 16:43:45 +0900

    20191212 - 더미 로그 쌓기 레어아바타 검색쪽에 추가

---
+++ Tradedun/src/main/java/com/thkong/tradedun/Common/dao/LogsDao.java
@@ -0,0 +1,10 @@
+package com.thkong.tradedun.Common.dao;
+
+import com.thkong.tradedun.Auction.vo.AvatarMastar;
+
+public interface LogsDao {
+
+	void insertRareAvatarSearch(AvatarMastar am);
+
+	
+}
