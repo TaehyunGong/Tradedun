158dac281eee69050cafedaca4793f6f2f9c1640
tony <tony950620@naver.com>
2019-12-12 13:53:23 +0900

    20191212 - 임포트 제거

--- Tradedun/src/main/java/com/thkong/tradedun/Board/controller/boardController.java
+++ Tradedun/src/main/java/com/thkong/tradedun/Board/controller/boardController.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
--- Tradedun/src/test/java/com/thkong/tradedun/main.java
+++ Tradedun/src/test/java/com/thkong/tradedun/main.java
@@ -2,8 +2,6 @@
 
 import java.io.IOException;
 
-import com.nhncorp.lucy.security.xss.XssPreventer;
-
 public class main {
 	
 //	ApplicationContext context = new GenericXmlApplicationContext("/SpringConfig/config/*.xml");
@@ -11,12 +9,7 @@ public class main {
 //	ObjectMapper mapper = context.getBean("mapper", ObjectMapper.class);
 //	SqlSession session = context.getBean("sqlSession", SqlSession.class);
 	
-	
 	public void process() throws IOException {
-		String dirty = "\"><script>alert('한글');</script>";
-	    String clean = XssPreventer.escape(dirty);
-	    
-	    System.out.println(clean);
 	}
 	
 	public static void main(String[] args) throws IOException {
